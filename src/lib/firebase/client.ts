2d2213fdab3836169ddcb77aa22b6285d430ca6d
Acassius Alves <acassiusalves@gmail.com>
2025-09-13 19:43:20 +0000

    pode atualizar la
    // Import the functions you need from the SDKs you n

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -5,19 +5,20 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyAJMoLSBriJDkNLYtjJ6-rJyaUK8x3sLG0",
+  authDomain: "studio-7321304121-9aa4d.firebaseapp.com",
+  projectId: "studio-7321304121-9aa4d",
+  storageBucket: "studio-7321304121-9aa4d.firebasestorage.app",
+  messagingSenderId: "470233078453",
+  appId: "1:470233078453:web:4fdf0696986f5d48e5fcfc"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
 const db = getFirestore(app);
-const functions = getFunctions(app);
+const functions = getFunctions(app, 'southamerica-east1');
 
 export { app, auth, db, functions };
