9006a44af39b64c01be69a25832cf5e572b0d189
Acassius Alves <acassiusalves@gmail.com>
2025-09-15 16:23:44 +0000

    deu certo

--- src/app/(driver)/layout.tsx
+++ src/app/(driver)/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import { MainLayout } from '@/components/layout/main-layout';
+
 
 export default function DriverLayout({
   children,
@@ -10,18 +10,8 @@ export default function DriverLayout({
 }) {
   
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted items-center justify-center">
-       <div className="text-center">
-        <h1 className="text-2xl font-bold mb-4">Acesso ao Painel</h1>
-        <p className="text-muted-foreground mb-6">
-          As verificações de permissão foram desativadas.
-        </p>
-        <Button asChild size="lg">
-          <Link href="/dashboard">
-            Ir para o Dashboard de Administração
-          </Link>
-        </Button>
-      </div>
-    </div>
+    <MainLayout>
+        {children}
+    </MainLayout>
   );
 }
