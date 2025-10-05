03f72d4f3fc99b7f5687b2f3477d3e2868d9dc0b
Acassius Alves <acassiusalves@gmail.com>
2025-10-05 17:31:08 +0000

    Try fixing this error: `Runtime Error: Error: CheckCircle2 is not define

--- src/app/(driver)/my-routes/[id]/page.tsx
+++ src/app/(driver)/my-routes/[id]/page.tsx
@@ -11,6 +11,7 @@ import {
   Clock,
   Milestone,
   PlayCircle,
+  CheckCircle2,
 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
